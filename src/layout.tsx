1f0b5da638b6cea47b65bc4ab86b6540502c1cea
colek <colek@lyft.com>
2023-04-02 19:35:58 -0400

    new form

---
+++ src/layout.tsx
@@ -0,0 +1,31 @@
+import { AppShell, MantineProvider } from '@mantine/core';
+import HeaderWithLinks from './headerWithLinks';
+import React from 'react';
+
+const Layout = (props: React.PropsWithChildren) => {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <AppShell
+        padding="md"
+        header={HeaderWithLinks({
+          links: [
+            { link: '/', label: 'Home' },
+            { link: '/about', label: 'About' },
+          ],
+        })}
+        styles={theme => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === 'dark'
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
+        })}>
+        {props.children}
+      </AppShell>
+      );
+    </MantineProvider>
+  );
+};
+
+export default Layout;
