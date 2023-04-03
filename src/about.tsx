770542671fabd5d1b3796b372eca4fa785f6ba1f
colek <colek@lyft.com>
2023-04-02 20:00:58 -0400

    init

---
+++ src/about.tsx
@@ -0,0 +1,13 @@
+import Layout from './layout';
+import { Text } from '@mantine/core';
+import React from 'react';
+
+const About = () => {
+  return (
+    <Layout>
+      <Text size="xl">Hello world</Text>
+    </Layout>
+  );
+};
+
+export default About;
---
+++ src/home.tsx
@@ -0,0 +1,13 @@
+import { Text } from '@mantine/core';
+import React from 'react';
+import Layout from './layout';
+
+const Home = () => {
+  return (
+    <Layout>
+      <Text size="xl">Hello world</Text>
+    </Layout>
+  );
+};
+
+export default Home;
